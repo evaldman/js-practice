b1ada9900e3ef1e03267ae49943630be70ca5279
Yevgeniy Valdman <evaldman@yahoo.com>
2022-03-22 14:14:33 -0400

    challenge1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,21 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
 
-40 + 8 + 23 - 10;
+// if (js === "amazing") alert("Javascript is FUN!");
+
+// 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+
+const massMark = 78
+const heightMark = 1.69
+const massJohn = 92
+const heightJohn = 1.95
+
+const bmiMark = massMark / heightMark ** 2
+const bmiJohn = massJohn / heightJohn ** 2
+
+console.log(bmiMark, bmiJohn)
