e25ca1dc538c8a5f27de92c1b560631c6ce44df3
Yevgeniy Valdman <evaldman@yahoo.com>
2022-03-22 16:51:32 -0400

    challenge2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -12,6 +12,7 @@ console.log(firstName);
 */
 
 
+
 const massMark = 78
 const heightMark = 1.69
 const massJohn = 92
@@ -24,6 +25,7 @@ const markHigherBMI = bmiMark > bmiJohn
 console.log(bmiMark, bmiJohn)
 console.log(markHigherBMI)
 
+/*
 const firstName = 'Jonas'
 const job = 'teacher'
 const birthYear = 1991
@@ -39,3 +41,30 @@ console.log("string with \n\multiple \n\lines")
 console.log(`string with
 multiple
 lines`)
+*/
+///////////// if / else
+
+const age = 15
+//const isOldEnough = age >= 18
+
+if(age >= 18) {
+    console.log("Sarah can start driving")
+} else {
+    const yearsLeft = 18 - age
+    console.log(`no license, wait ${yearsLeft} more years`)
+}
+
+const birthYear = 1991
+let century
+if (birthYear <= 2000) {
+    century = 20
+} else {
+    century = 21
+}
+console.log(century)
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${bmiMark}) is higher`)
+} else {
+    console.log(`Johns's BMI (${bmiJohn}) is higher`)
+}
