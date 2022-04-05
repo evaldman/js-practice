95f7972413ee0394f36ebd18657ff9e4680063ef
Yevgeniy Valdman <evaldman@yahoo.com>
2022-04-05 10:41:24 -0400

    arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -142,3 +142,53 @@ scoreKoalas = calcAverage(23, 34, 27);
 checkWinner(scoreDolphins, scoreKoalas); // test data 2
 
 console.log("_______________");
+
+/////// arrays ////////
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years1 = new Array(1991, 1984, 2008, 2020);
+console.log(years1);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // = [2] = peter
+
+// can change an element in the array:
+friends[2] = "Jay";
+console.log(friends);
+// but not the whole array:
+// friends = ["Bob", "Alice"]
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtman", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+
+// Exercise:
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+// can't do this with arrays:
+console.log(calcAge(years)); // = NaN
+// can do:
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+// can put function calls inside an array
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
