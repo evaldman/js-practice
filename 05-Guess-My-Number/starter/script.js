1a772cb9b427b1df0f3e77ef37300f348f834693
Yevgeniy Valdman <evaldman@yahoo.com>
2022-04-08 16:22:44 -0400

    click event

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".message").textContent = "Correct Number!";
@@ -10,3 +10,20 @@ document.querySelector(".number").textContent = 13;
 document.querySelector(".score").textContent = 10;
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+*/
+
+////////// event listeners ////////
+
+document.querySelector(".check").addEventListener("click", function () {
+  //   console.log(document.querySelector(".guess").value);
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  }
+});
+
+// const checkBtn = document.querySelector(".check");
+// checkBtn.addEventListener("click", function () {
+//   console.log(document.querySelector(".guess").value);
+// });
