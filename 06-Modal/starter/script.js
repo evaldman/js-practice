d596a0383724d929a496ebaa095dba92a8c0e126
Yevgeniy Valdman <evaldman@yahoo.com>
2022-04-11 11:52:59 -0400

    modal close

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,5 +7,25 @@ const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  //   console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+  function closeModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+  btnCloseModal.addEventListener("click", function () {
+    closeModal();
+    // modal.classList.add("hidden");
+    // overlay.classList.add("hidden");
+  });
+  overlay.addEventListener("click", function () {
+    closeModal();
+    // modal.classList.add("hidden");
+    // overlay.classList.add("hidden");
+  });
 }
