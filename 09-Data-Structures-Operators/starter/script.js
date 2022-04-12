e9bf3f8ad8c0401d7540d4adfbbafb81577ab4ec
Yevgeniy Valdman <evaldman@yahoo.com>
2022-04-12 19:42:04 -0400

    destruct arr

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   openingHours: {
     thu: {
@@ -26,4 +26,47 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr; // destructuring (unpacking) of array, makes them separate variables, original array is not affected
+console.log(x, y, z);
+console.log(arr);
+
+const [first, second] = restaurant.categories;
+let [main, , secondary] = restaurant.categories;
+console.log(first, second); // italian pizzaria
+console.log(main, secondary); // italian vegetarian
+
+// switch variables:
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary); // vegetarian italian
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary); // vegetarian italian
+
+// receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// nested destructuring
+const nested = [2, 4, [5, 6]];
+const [p, , q] = nested;
+console.log(p, q);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values (don't know the length of the array)
+// const [r, s, t] = [8, 9];
+// console.log(r, s, t); // 8 9 undefined
+const [r = 1, s = 1, t = 1] = [8, 9];
+console.log(r, s, t); // 8 9 1
