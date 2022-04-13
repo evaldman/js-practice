c81f6832ac8460d2ec60d1f7180ee4bb43b3bc1d
Yevgeniy Valdman <evaldman@yahoo.com>
2022-04-13 13:03:26 -0400

    rest

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -42,6 +42,10 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 ////////// destructuring arrays ////////
@@ -184,3 +188,45 @@ console.log(newRestaurant);
 const restaurantCopy = { ...restaurant };
 restaurantCopy.name = "Ristorante Roma";
 console.log(restaurantCopy);
+
+/////// rest pattern and parameters ////////
+console.log("---------rest pattern-----------");
+
+// destructuring //
+
+// spread, because on right side of =
+const arr3 = [1, 2, ...[3, 4]];
+// rest, because on left side of =
+const [a1, b1, ...others] = [1, 2, 3, 4, 5];
+// takes the rest of the elements (remainig elements of array) and puts them into new array (others) - collects the elements that are unused in the destructuring assignment.. rest pattern should always be the last element
+console.log(a1, b1, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// objects
+const { sat, ...weekdays } = restaurant.openingHours;
+// weekdays becomes its own new object
+console.log(weekdays);
+
+// functions //
+
+const add = function (...numbers) {
+  // rest parameters - takes all the numbers and compresses them into one array
+  // console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x1 = [23, 5, 7];
+add(...x1);
+
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
+restaurant.orderPizza("mushrooms");
