1c788cac658d13788be2485f3f15b1afb2e1b7a3
Yevgeniy Valdman <evaldman@yahoo.com>
2022-04-20 00:10:34 -0400

    call/apply

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -115,3 +115,54 @@ greet("Hello")("Steve");
 // mini challenge
 const greet2 = greeting => name => console.log(`${greeting} ${name}`);
 greet2("Hello")("Frank");
+
+console.log("----------call and apply methods----------");
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  //book: function(){}
+  // new syntax:
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+lufthansa.book(239, "Bob Smith");
+lufthansa.book(635, "Mike Jones");
+console.log(lufthansa);
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+// book(23, "Sara Wills"); // undefined, because this keyword is no longer linked ot lufthansa, this makes a copy and becoes a function not a method in an object.
+
+// call method
+book.call(eurowings, 23, "Sara Wills"); // call() method calls a method of an object, substituting another object for the current object
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary "Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 583, "Mary Coop");
+
+// apply method
+const flightData = [583, "George Cooper"];
+book.apply(swiss, flightData);
+console.log(swiss);
+//  -- not used much in modern javascript
+// its preferred to use call() method with spread operator
+book.call(swiss, ...flightData);
