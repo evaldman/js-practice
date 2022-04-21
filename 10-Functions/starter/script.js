e6307fb80728e3b6e40998bedb169ca08282214f
Yevgeniy Valdman <evaldman@yahoo.com>
2022-04-21 00:14:45 -0400

    challenge1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -260,9 +260,21 @@ const poll = {
       alert("Pick between 0 and 3");
       this.registerNewAnswer();
     }
-    console.log(this.answers);
+    // console.log(this.answers);
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults: function (type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
   },
 };
 document
   .querySelector(".poll")
   .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
