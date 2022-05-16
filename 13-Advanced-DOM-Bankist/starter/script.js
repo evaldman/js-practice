af788291907fec1a70c6ac4e317c3d924b7198dd
Yevgeniy Valdman <evaldman@yahoo.com>
2022-05-16 10:59:43 -0400

    lDOM lifecycle

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -532,3 +532,18 @@ console.log(h1.parentElement.children);
 //     el.style.transform = "scale(0.5)";
 //   }
 // });
+
+console.log("-----lifecycle DOM events-----");
+document.addEventListener("DOMContentLoaded", function (e) {
+  console.log("HTML parsed and DOM tree built", e);
+});
+
+window.addEventListener("load", function (e) {
+  console.log("page fully loaded", e);
+});
+
+// window.addEventListener("beforeunload", function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = "";
+// });
