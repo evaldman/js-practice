8d960a69e23503d04ba422a721aa801c7fb45694
Yevgeniy Valdman <evaldman@yahoo.com>
2022-05-17 21:43:10 -0400

    proto inherit

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -88,3 +88,26 @@ console.log(jonas.species);
 // jonas has own properties of name and birthyear and inherited properties of calcAge and species
 console.log(jonas.hasOwnProperty("firstName")); // true
 console.log(jonas.hasOwnProperty("species")); // false
+
+console.log("----- prototypal inheritance -----");
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__); // Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__.__proto__); // null
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+// can create our own array method and call it on any array that we want (not recommended to do, just an example of what can be done)
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir(h1);
+
+console.dir(x => x + 1);
