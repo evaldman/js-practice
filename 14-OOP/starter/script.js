381e8cefbca9c7077b5a47f33512b88d5a1d0310
Yevgeniy Valdman <evaldman@yahoo.com>
2022-05-17 21:43:35 -0400

    challenge1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -111,3 +111,43 @@ const h1 = document.querySelector("h1");
 console.dir(h1);
 
 console.dir(x => x + 1);
+
+console.log("----- challenge 1 -----");
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+// 1.
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+// 2.
+Car.prototype.accelerate = function () {
+  console.log(this.speed + 10);
+};
+
+// 3.
+Car.prototype.brake = function () {
+  console.log(this.speed - 5);
+};
+
+// 4.
+const beemer = new Car("BMW", 120);
+const benzo = new Car("Mercedes", 95);
+
+console.log(beemer, benzo);
+beemer.accelerate();
+beemer.brake();
+benzo.accelerate();
+benzo.brake();
