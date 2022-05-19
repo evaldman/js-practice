0f60d493f720a88be777b7faca21d3e6427a013c
Yevgeniy Valdman <evaldman@yahoo.com>
2022-05-18 20:38:25 -0400

    Obj,createe inherit

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -477,3 +477,30 @@ const martha = new StudentClass("Martha Jones", 2012, "Computer Science");
 console.log(martha);
 martha.introduce();
 martha.calcAge();
+
+console.log("----- class inherit Object.create -----");
+// const PersonProto = {
+//     calcAge() {
+//       console.log(2037 - this.birthYear);
+//     },
+//     // function can have any name
+//     init(firstName, birthYear) {
+//       this.firstName = firstName;
+//       this.birthYear = birthYear;
+//     },
+//   };
+//   const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+console.log(jay);
+jay.introduce();
+jay.calcAge();
