8b129e86e1344caae219cca7778a51f2e0233cd4
Yevgeniy Valdman <evaldman@yahoo.com>
2022-05-18 20:59:47 -0400

    more class ex

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -504,3 +504,48 @@ jay.init("Jay", 2010, "Computer Science");
 console.log(jay);
 jay.introduce();
 jay.calcAge();
+
+console.log("----- class example -----");
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+  // public interface (API)
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log("Loan approved");
+    }
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+// instead of manually manipulating objects, its better to create methods: deposit, withdraw
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1);
