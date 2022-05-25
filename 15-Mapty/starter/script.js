d22fbd3b0cfef1904f215f6594237156226d24a8
Yevgeniy Valdman <evaldman@yahoo.com>
2022-05-24 21:45:40 -0400

    map marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -25,16 +25,34 @@ if (navigator.geolocation)
 
       const coords = [latitude, longitude];
       const map = L.map("map").setView(coords, 13);
+      //   console.log(map);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      L.marker(coords).addTo(map).bindPopup("Current Location").openPopup();
+
+      // event created by leaflet, not available outside of leaflet
+      map.on("click", function (mapEvent) {
+        // console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 150,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("workout")
+          .openPopup();
+      });
     },
     function () {
       alert("Could not get your position");
